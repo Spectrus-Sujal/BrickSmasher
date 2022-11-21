894d71111b5689fc0256a36a4d53f26fd07faf09
Sujal Naroia <sujalnaroia0099@gmail.com>
2022-11-20 19:14:06 -0600

    Added comments to player.h and player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,49 +1,38 @@
 ﻿
 #include "player.h"
 
-/**
- * \brief Constructor for Player using Point from point.h
- * \param c Coordinates x,y
- * \param si Size x,y
- * \param sp Horizontal Speed
- */
+// Call the constructor from Paddle in paddle.h
 Player::Player(const Point& c, const Point& si, double sp) : Paddle{c, si}, speed{sp} {}
 
-/**
- * \brief Constructor for Player using only ints
- * \param cx Coordinate x
- * \param cy Coordinate y
- * \param six Size x
- * \param siy Size y
- * \param sp Horizontal Speed
- */
 Player::Player(double cx, double cy, double six, double siy, double sp) : Paddle { cx, cy, six, siy }
 {
 	speed = sp;
 }
 
-/**
- * \brief Move the player on the Left and Right on the X axis
- * \param moveLeft is used to know is the player is moving right of left
- */
 void Player::move(bool moveLeft)
 {
 	int direction = 1;
 
+	// Check if the player is moving left
 	if(moveLeft)
 	{
 		direction = -1;
 	}
 
+	// Call changeX in Paddle
+	// Change in X being based on direction adn speed
 	changeX(speed * direction);
 
+	// Reset the Player X if it is further than the edge
 	if (getX() < 0)
 	{
+		// Change it based on how much left edge exceeds 0
 		changeX(0 - getX());
 	}
 
 	if (getX() + getSizeX() > ofGetWidth())
 	{
+		// Change based on how much the right edge exceeds the Width
 		changeX(ofGetWidth() - (getX() + getSizeX()));
 	}
 
@@ -51,39 +40,42 @@ void Player::move(bool moveLeft)
 
 void Player::move(int x)
 {
+	// Get the center X of the player
 	int halfSize = (getSizeX() / 2);
+	// Check is the new Player X is within the the canvas bounds
 	if ((x + halfSize <= ofGetWidth()) && x - halfSize >= 0)
 	{
+		// Change the X based on the center
 		changeX(x - (getX() + halfSize));
 	}
 }
 
-
-/**
- * \brief Method used to decrease the player's size by half
- */
 void Player::half()
 {
+	// Check if size was already half before
 	if(!isHalf)
 	{
+		// Call changeSizeX from Paddle
 		changeSizeX( -(getSizeX() / 2));
+		// Set isHalf to true
 		isHalf = true;
 	}
 }
 
-/**
- * \brief Draw the player as a rectangle on the canvas using Coordinate and Size
- */
 void Player::drawPlayer() const
 {
+	// Set the Color of the player
 	ofSetColor(playerColor);
+	// Draw the player from the top left
 	ofDrawRectangle(getX(), getY(), getSizeX(), getSizeY());
 }
 
-void Player::checkCollision(Ball& ball)
+void Player::checkCollision(Ball& ball) const
 {
+	// Call checkCollisions from Paddle
 	if (Paddle::checkCollision(ball))
 	{
-		Paddle::doCollision(ball);
+		// Call doCollisions in Paddle
+		doCollision(ball);
 	}
 }
--- src/player.h
+++ src/player.h
@@ -6,8 +6,9 @@
 
 /**
  * \brief Player class which is used to contain all of the information of the player
+ *		derived from Paddle in paddle.h
  */
-class Player : Paddle
+class Player : public Paddle
 {
 private:
 	double speed;
@@ -17,17 +18,49 @@ private:
 
 public:
 
+	/**
+	* \brief Constructor using Point from point.h
+	* \param c Coordinates x,y
+	* \param si Size x,y
+	* \param sp Horizontal Speed
+	*/
 	Player(const Point &c, const Point &si, double sp);
+
+	/**
+	* \brief Constructor using doubles
+	* \param cx Coordinate x
+	* \param cy Coordinate y
+	* \param six Size x
+	* \param siy Size y
+	* \param sp Horizontal Speed
+	*/
 	Player(double cx, double cy, double six, double siy, double sp);
 
+	/**
+	* \brief Move the player Left and Right on the X axis
+	* \param moveLeft know if player is moving left
+	*/
 	void move(bool moveLeft);
 
+	/**
+	 * \brief Move the player to a new X position
+	 * \param x New X position of type int
+	 */
 	void move(int x);
 
+	/**\brief Decrease the player's size by half only Once */
 	void half();
 
+	/**
+	*\brief Draw the player as a rectangle on
+	*		the canvas using Coordinate and Size
+	*/
 	void drawPlayer() const;
 
-	void checkCollision(Ball& ball);
+	/**
+	 * \brief Check if the ball is colliding with the palyer
+	 * \param ball Object of type Ball from ball.h
+	 */
+	void checkCollision(Ball& ball) const;
 
 };
