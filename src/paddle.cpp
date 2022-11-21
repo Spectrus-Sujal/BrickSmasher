8c8fd78d1fc9303fce137beeadfe715d4343fe7a
Sujal Naroia <sujalnaroia0099@gmail.com>
2022-11-20 18:48:00 -0600

    Added comments to paddle.h and paddle.cpp

--- src/paddle.cpp
+++ src/paddle.cpp
@@ -1,12 +1,9 @@
 #include "paddle.h"
 
+////////////// Constructors ///////////////////////
 Paddle::Paddle(Point c, Point s) : coordinate{ c }, size{ s } {}
 
-Paddle::Paddle(double cx, double cy, double six, double siy)
-{
-	coordinate = Point{ cx, cy };
-	size = Point{ six, siy };
-}
+Paddle::Paddle(double cx, double cy, double six, double siy) : coordinate{cx, cy}, size{six, siy} {}
 
 double Paddle::getX() const
 {
@@ -41,26 +38,32 @@ void Paddle::changeSizeX(int delta)
 
 bool Paddle::checkCollision(const Ball& ball) const
 {
-	return ((ball.getX() + ball.getRadius() >= getX()) && (ball.getY() + ball.getRadius() >= getY())) &&
-		((ball.getX() - ball.getRadius() <= getX() + getSizeX()) && (ball.getY() - ball.getRadius() <= getY() + getSizeY()));
+	// Return if the ball is within the area where the Paddle is drawn
+	return ((ball.getX() + ball.getRadius() >= getX()) 
+		&& (ball.getY() + ball.getRadius() >= getY()))
+		&&((ball.getX() - ball.getRadius() <= getX() + getSizeX()) 
+		&& (ball.getY() - ball.getRadius() <= getY() + getSizeY()));
 }
 
 void Paddle::doCollision(Ball& ball) const
 {
-	if (ball.getX() + ball.getRadius() > getX() + getSizeX())
-	{
-		ball.flipX();
-	}
-	else if (ball.getX() - ball.getRadius() < getX())
+	// Check if the ball is touching either left or right edge
+
+	// Checking if the right edge of the ball is greater than
+	// the right edge of the Paddle
+
+	// or
+
+	// if the left edge of the ball is less than
+	// the left edge of the Paddle
+	if (ball.getX() + ball.getRadius() > getX() + getSizeX() || ball.getX() - ball.getRadius() < getX())
 	{
 		ball.flipX();
 	}
 
-	if (ball.getY() + ball.getRadius() > getY() + getSizeY())
-	{
-		ball.flipY();
-	}
-	else if (ball.getY() - ball.getRadius() < getY())
+	// Do the same but for the top and bottom the ball
+	// and the Paddle
+	if ((ball.getY() + ball.getRadius() > getY() + getSizeY()) || (ball.getY() - ball.getRadius() < getY()))
 	{
 		ball.flipY();
 	}
--- src/paddle.h
+++ src/paddle.h
@@ -2,32 +2,94 @@
 
 #include "point.h"
 #include "ball.h"
-#include "ofGraphics.h"
 
+/**
+ * \brief A class that contains the X and Y position of an object
+ *		as well as its size which can be used to check and
+ *		do collisions
+ */
 class Paddle
 {
 private:
-
+	/**
+	 * \brief X and Y position
+	 */
 	Point coordinate;
+
+	/**
+	 * \brief Width and Height
+	 */
 	Point size;
 
 public:
 
-	//Constructor
+////////////// Constructors ///////////////////////
+	/**
+	 * \brief Constructor using Point from point.h
+	 * \param c X and Y position
+	 * \param s Width and Height
+	 */
 	Paddle(Point c, Point s);
+
+	/**
+	 * \brief Constructor using doubles
+	 * \param cx X position
+	 * \param cy Y position
+	 * \param six Width
+	 * \param siy Height
+	 */
 	Paddle(double cx, double cy, double six, double siy);
 
-	// Getters
+////////////// Getters ///////////////////////
+	/**
+	 * \brief Get the X position of Paddle
+	 * \return Value of type double
+	 */
 	double getX() const;
+
+	/**
+	 * \brief Get the Y position of Paddle
+	 * \return Value of type double
+	 */
 	double getY() const;
+
+	/**
+	 * \brief Get the Width of Paddle
+	 * \return Value of type double
+	 */
 	double getSizeX() const;
+
+	/**
+	 * \brief Get the Height of Paddle
+	 * \return Value of type double
+	 */
 	double getSizeY() const;
 
+	/**
+	 * \brief Move the Paddle horizontally
+	 * \param delta Change in X position
+	 */
 	void changeX(int delta);
+
+	/**
+	 * \brief Change the Paddle Width
+	 * \param delta Change in Width
+	 */
 	void changeSizeX(int delta);
 
-	//Collision
+////////////// Collision ///////////////////////
+	/**
+	 * \brief Check whether the ball is colliding with the Paddle
+	 * \param ball Object of type Ball from ball.h
+	 * \return if Paddle and ball are colliding
+	 */
 	bool checkCollision(const Ball& ball) const;
+
+	/**
+	 * \brief Change the ball speed based on where it
+	 *		collided with the Paddle to simulate a bounce
+	 * \param ball Object of type Ball from ball.h
+	 */
 	void doCollision(Ball& ball) const;
 
 };
