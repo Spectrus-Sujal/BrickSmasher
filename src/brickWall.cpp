808f73da53dd272b7231c5fe0b45551615aaa86c
Sujal Naroia <sujalnaroia0099@gmail.com>
2022-11-20 18:01:48 -0600

    Added comments to brickWall.h and brickWall.cpp

--- src/brickWall.cpp
+++ src/brickWall.cpp
@@ -5,27 +5,37 @@ BrickWall::BrickWall(std::vector<Brick> &w) : wall{ w } {}
 
 void BrickWall::drawBricks()
 {
+	// Go through each Brick in wall
 	for each (Brick b in wall)
 	{
+		// Get the color
 		ofSetColor(b.getBrickColor());
+		// Draw a rectangle from the top left
 		ofDrawRectangle(b.getX(), b.getY(), b.getSizeX(), b.getSizeY());
 	}
 }
 
-void BrickWall::checkCollision(Ball &ball, Player& player)
+void BrickWall::checkCollision(Ball &ball)
 {
+	// Loop through every Brick
 	for (auto b{ 0 }; b < wall.size(); ++b)
 	{
+		// Check if the ball is colliding with anything
 		if (wall[b].checkCollision(ball))
 		{
+			// Bounce the ball using doCollision in paddle.h
 			wall[b].doCollision(ball);
 
+			// Increment the amount of Bricks Destroyed
 			bricksDestroyed++;
 
+			// Update lastBrickDestroyed
 			lastBrickDestroyed = wall[b];
 
+			// Remove the Brick
 			wall.erase(wall.begin() + b);
 
+			// top the loop
 			break;
 		}
 	}
@@ -38,6 +48,7 @@ Brick BrickWall::getLastDestroyedBrick() const
 
 void BrickWall::resizeWalls(const std::vector<Brick>& w)
 {
+	// Assign w to wall
 	wall = w;
 }
 
--- src/brickWall.h
+++ src/brickWall.h
@@ -2,29 +2,55 @@
 
 #include "brick.h"
 #include "ball.h"
-#include "brick.h"
-#include "player.h"
-#include "ofGraphics.h"
 
+/**
+ * \brief Varrying size array of Bricks from brick.h
+ */
 class BrickWall
 {
 private:
-
+	/**\brief Collection of Bricks*/
 	std::vector<Brick> wall;
+	/**\brief Amount of Bricks removed from wall */
 	int bricksDestroyed = 0;
 
+	/**\brief Value of the last Brick removed from wall */
 	Brick lastBrickDestroyed {0, 0, 0, 0, Brick::level::Yellow};
 
 public:
 
+///////////////// Constructors ///////////////////////
+
+	/**
+	 * \brief Constructor for BrickWall
+	 * \param w Vector of type Brick from brick.h
+	 */
 	BrickWall(std::vector<Brick>& w);
 
+	/**\brief Draw Bricks in wall onto the canvas */
 	void drawBricks();
 
+	/**
+	 * \brief Access the amount of Bricks missing from wall
+	 * \return Value of type int
+	 */
 	int getBricksDestroyed() const;
+
+	/**
+	 * \brief Access the last Brick removed from wall
+	 * \return Value of type Brick
+	 */
 	Brick getLastDestroyedBrick() const;
 
+	/**
+	 * \brief Replace wall with a new vector
+	 * \param w vector of type Brick
+	 */
 	void resizeWalls(const std::vector<Brick>& w);
 
-	void checkCollision(Ball& ball, Player& player);
+	/**
+	 * \brief Check if the ball is colliding with any Bricks present in wall
+	 * \param ball Variable of type Ball from ball.h
+	 */
+	void checkCollision(Ball& ball);
 };
