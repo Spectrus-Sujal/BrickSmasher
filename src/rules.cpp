bd88e0eac8dc3c4ee667c7f844638da0617d4328
Sujal Naroia <sujalnaroia0099@gmail.com>
2022-11-20 21:14:34 -0600

    Added comments to rules.h and rules.cpp

--- src/rules.cpp
+++ src/rules.cpp
@@ -5,49 +5,67 @@ Rules::Rules(){}
 
 void Rules::update(BrickWall& bw, Ball& ball, Player& player)
 {
+	// Check the balls new position
 	checkBall(ball, player);
 
+	// Check if a brick was destroyed
 	if(destroyedBricks != bw.getBricksDestroyed())
 	{
 		checkBricks(bw, ball, player);
 	}
 
+	// Check if the game has ended
 	checkState();
 }
 
+// Check if the ball meets any conditions
+// If so then update the other objects in the game
+
 void Rules::checkBall(Ball& ball, Player& player)
 {
+	// Check if this is the first time the ball is bouncing at the top edge
 	if (firstTopBounce && ball.getY() - ball.getRadius() <= 0)
 	{
+		// Half the player size
 		player.half();
+		// Make sure this condition doesn't trigger again
 		firstTopBounce = false;
 	}
 
+	// Check if the ball has touched the bottom
 	if(ball.getY() + ball.getRadius() >= ofGetHeight())
 	{
+		// Decrement the lives
 		lives--;
+		// Respawn the ball
 		ball.spawnBall(ofGetWidth() / 2, ofGetHeight() / 2);
 	}
 }
 
 void Rules::checkBricks(BrickWall& bw, Ball& ball,Player& player)
 {
+	// Update the amount of destroyed bricks
 	destroyedBricks = bw.getBricksDestroyed();
 
+	// Check if a milestone is reached
 	if (destroyedBricks < 13)
 	{
 		switch (destroyedBricks)
 		{
 			case 4:
 			case 12:
+				// Update the ball speed every time
 				ball.increaseSpeed();
 				break;
 		}
 	}
 
+	// Check if a certain brick is destroyed for the first time
 	if (firstOrange && bw.getLastDestroyedBrick().getLvl() == Brick::level::Orange)
 	{
+		// Increase the ball speed
 		ball.increaseSpeed();
+		// Make sure it doesn't repeat
 		firstOrange = false;
 	}
 	else if (firstRed && bw.getLastDestroyedBrick().getLvl() == Brick::level::Red)
@@ -56,6 +74,7 @@ void Rules::checkBricks(BrickWall& bw, Ball& ball,Player& player)
 		firstRed = false;
 	}
 
+	// Increase points based on destroyed brick
 	increasePoints(bw.getLastDestroyedBrick().getPoints());
 }
 
@@ -67,8 +86,10 @@ bool Rules::getIsPlaying() const
 
 void Rules::checkState()
 {
+	// Check if the game is ending
 	if(lives <= 0)
 	{
+		// Call a method based on how it ended
 		loseGame();
 	}
 
@@ -80,6 +101,7 @@ void Rules::checkState()
 
 void Rules::loseGame()
 {
+	// Change the end Satus and end the game
 	endStatus = "Lost\n";
 	isPlaying = false;
 }
@@ -97,12 +119,15 @@ void Rules::increasePoints(int delta)
 
 void Rules::showStats() const
 {
+	// Display a tect message stating the Point and Lives
+	// In the bottom left corner
 	ofDrawBitmapString("Scores: " + std::to_string(points), 0, ofGetHeight() * 0.90);
 	ofDrawBitmapString("Lives: " + std::to_string(lives), 0, ofGetHeight() * 0.95);
 }
 
 void Rules::displayScreen(bool instructions) const
 {
+	// Check if instructions are being displayed or not
 	if(instructions)
 	{
 		displayInstructions();
@@ -110,23 +135,25 @@ void Rules::displayScreen(bool instructions) const
 	else
 	{
 		ofDrawBitmapString("You have " + endStatus, ofGetWidth() * 0.45, ofGetHeight() * 0.40);
-		ofDrawBitmapString("Press S to start the game", ofGetWidth() * 0.25, ofGetHeight() * 0.70);
+		ofDrawBitmapString("Press Play to start the game", ofGetWidth() * 0.25, ofGetHeight() * 0.70);
 		ofDrawBitmapString("Press I to see Instructions", ofGetWidth() * 0.65, ofGetHeight() * 0.70);
 	}
 }
 
 void Rules::displayInstructions() const
 {
+	// Show the Instructions
 	ofDrawBitmapString("Use A and D to move the paddle left and right ", ofGetWidth() * 0.20, ofGetHeight() * 0.10);
 	ofDrawBitmapString("You can also move the mouse to move the paddle left and right", ofGetWidth() * 0.20, ofGetHeight() * 0.30);
-	ofDrawBitmapString("Try to destroy all the bricks byb bouncing the ball off of the paddle", ofGetWidth() * 0.20, ofGetHeight() * 0.50);
+	ofDrawBitmapString("Try to destroy all the bricks by bouncing the ball off of the paddle", ofGetWidth() * 0.20, ofGetHeight() * 0.50);
 	ofDrawBitmapString("Make sure to not let the ball touch the ground as you will lose a life", ofGetWidth() * 0.20, ofGetHeight() * 0.70);
-	ofDrawBitmapString("Press S to start the game", ofGetWidth() * 0.20, ofGetHeight() * 0.90);
+	ofDrawBitmapString("Press Play to start the game", ofGetWidth() * 0.20, ofGetHeight() * 0.90);
 }
 
 
 void Rules::startGame()
 {
+	// Reset all the values
 	isPlaying = true;
 	points = 0;
 	lives = 3;
--- src/rules.h
+++ src/rules.h
@@ -4,46 +4,124 @@
 #include "player.h"
 #include "brickWall.h"
 
+/**
+ * \brief A Class that contains all the rules that dictate the flow of this game.
+ *		This includes all the conditions that need to be fulfilled in order for certain
+		events to change what happens in the game; This includes the game Starting, Ending, 
+		the ball getting faster and the player's size decreasing.
+ */
 class Rules
 {
 private:
 
+	/**
+	* \brief The first time the ball bounces
+			off of this
+	*/
+	bool firstOrange {true}, firstRed {true}, firstTopBounce{ true };
+
+	/**
+	* \brief Current state of the game
+	*/
 	bool isPlaying {false};
 
-	bool firstTopBounce{ true };
-
+	/**
+	* \brief Number of Bricks the player has sestroyed
+	*/
 	int destroyedBricks{ 0 };
 
-	bool firstOrange {true};
-	bool firstRed {true};
-
-	int lives{ 3 };
+	/**
+	* \brief The players points based on number and
+		kind of Bricks destroyed
+	*/
 	int points{ 0 };
 
+	/**
+	* \brief Lives left of player
+	*/
+	int lives{ 3 };
+
+	/**
+	* \brief Which state the game ended
+	*/
 	std::string endStatus{ "Begun" };
 
+	/**
+	* \brief Increase the players points
+	* \param delta: Change in points
+	*/
 	void increasePoints(int delta);
 
+	/**
+	* \brief Check if any conditions are met
+	* \param ball: Object of type Ball from ball.h
+	* \param player: Object of type Player from player.h
+	*/
 	void checkBall(Ball& ball, Player& player);
+
+	/**
+	* \brief Check if the Bricks fullfill and conditions
+	* \param bw: Object of type BrickWall from brickWall.h
+	* \param ball: Object of type Ball from ball.h
+	* \param player: Object of type PLayer from player.h
+	*/
 	void checkBricks(BrickWall& bw, Ball& ball, Player& player);
+
+	/**
+	* \brief Check if the game has ended and if so
+	*		has the player won or lost
+	*/
 	void checkState();
 
-	void loseGame();
-	void winGame();
+	/**
+	* \brief Change end and game status
+	*/
+	void loseGame(), winGame();
 
 public:
 
+////////////////// Constructor //////////////
+
+	/**
+	* \brief Default Constructor
+	*/
 	Rules();
 
+////////////////// Getters //////////////////	
+
+	/**
+	* \brief Get whether the game is still going or no
+	* \return Value of type bool
+	*/
 	bool getIsPlaying() const;
 
+	/**
+	* \brief Display points and lives on the Canvas
+	*/
 	void showStats() const;
 
+	/**
+	* \brief Display Start Screen
+	* \param instructions: Change to instruction screen
+	*/
 	void displayScreen(bool instructions) const;
+
+	/**
+	* \brief Display Instrcution Screen
+	*/
 	void displayInstructions() const;
 
+	/**
+	* \brief Reset all variables and conditions as if the game is restarting
+	*/
 	void startGame();
 
+	/**
+	* \brief Check for changes in the game and whether any game changing conditions have been met
+	* \param bw: Object of type BrickWall from brickWall.h
+	* \param ball: Object of type Ball from ball.h
+	* \param player: Object of type Player from player.h
+	*/
 	void update(BrickWall& bw, Ball& ball, Player& player);
 
 };
